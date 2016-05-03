291e04c74b0d7c7fa89695456fdaea03e57d6567
Stephen Morrison <srmorrisonjit@gmail.com>
2016-05-03 17:46:50 -0400

    add initial SQL creation script and fallback settings

---
+++ exampleFallbackSettings.js
@@ -0,0 +1,6 @@
+exports.REDIS_SECRET = 'Rx#z(ANYRyp9NjfLp62gsgVbF';
+exports.REDIS_HOST = 'localhost';
+exports.REDIS_PORT = 7658;
+exports.REDIS_AUTH = 'dZGVorD42Up/HWDU2n>RsfcZN';
+exports.FRONTEND_PORT = 4000;
+exports.API_URL = 'http://localhost:5050';
