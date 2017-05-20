3edd84d16efd3bca76ab5dc03d7330979ce47c05
Alan Romano <alan.rom25.95@gmail.com>
2017-05-20 15:05:42 -0400

    Add new EmptySystem sql script

--- exampleFallbackSettings.js
+++ exampleFallbackSettings.js
@@ -3,4 +3,5 @@ exports.REDIS_HOST = 'localhost';
 exports.REDIS_PORT = 7658;
 exports.REDIS_AUTH = 'dZGVorD42Up/HWDU2n>RsfcZN';
 exports.FRONTEND_PORT = 4000;
-exports.API_URL = 'http://localhost:5050';
+
+///#### Remember to set exports.API_URL in server/utils/react_constants.js ####
